943f76472fa0af06c11a4d351f45e779cba604db
paverdugo <113260823+paverdugo@users.noreply.github.com>
2026-08-03 22:01:31 -0400

    Update supabaseClient.js
    intento de coneccion

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,4 @@
 import { createClient } from '@supabase/supabase-js';
-
 const supabaseUrl = 'https://zxfgieikzahwycyzkkgn.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inp4ZmdpZWlremFod3ljeXpra2duIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUxODUwODAsImV4cCI6MjEwMDc2MTA4MH0.B1r-I9rmSPs4DO2UM6RuljIZsUoCC1ekVlbc39ZSD0s';
-
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
